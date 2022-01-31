4a46dc371d36a33baf60c963ed3f18043c2fe2da
yc508 <yicongchen98@gmail.com>
2022-01-31 16:45:00 -0500

    Started a test-driven function

---
+++ test_slope_intercept.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.mark.parametrize("tuple1, tuple2, input, expected", [
+    [(1,4), (2,6), 5, 12]
+])
+
+
+
+def test_linearequation(tuple1, tuple2, input, expected):
+    from slope_intercept import linearequation
+    answer = linearequation(tuple1, tuple2, input)
+    assert answer == int(expected)
