f0400489279583da455eae5c4dff1f1ed0d7fef5
yc508 <yicongchen98@gmail.com>
2022-02-02 16:20:54 -0500

    modified testing and installed github action

--- test_weight_entry.py
+++ test_weight_entry.py
@@ -4,6 +4,11 @@ import pytest
 @pytest.mark.parametrize("input, expected", [
     ("22 lb", 10),
     ("50 kg", 50),
+    ("23.5 kg", 24),
+    #("22lb", 10),
+    ("22 Lb", 10),
+    ("22 LB", 10),
+    #("-22 lb", 10),
     ])
 def test_parse_weight_input(input, expected):
     from weight_entry import parse_weight_input
--- weight_entry.py
+++ weight_entry.py
@@ -1,15 +1,16 @@
 def input_weight_entry():
     print("Enter patient weight in form of ## units (e.g., 105 lb)")
     weight_input = input("Enter weight: ")
-    weight_in_kg = parse_weight_input(weight_input)
+    weight_in_kg = parse_weight_input(weight_input)#takes the string
     print("The patient weight of {} kg will be stored "
           "in database.".format(weight_in_kg))
 
 
 def parse_weight_input(weight_input):
     weight, units = weight_input.split(' ')
-    weight = int(weight)
-    if units == "lb":
+    weight = float(weight)
+    units = units.lower()
+    if units in ["lb", "lbs", "pound"]:
         weight_kg = convert_lb_to_kg(weight)
     else:
         weight_kg = weight
